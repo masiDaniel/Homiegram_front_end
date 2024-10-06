ee905d7e0ed418b0b070e02f0488eb1c21de83dc
masiDaniel <masidaniel02@gmail.com>
2024-10-06 20:12:17 +0300

    seperation of concerns

--- HomieGramBackEnd/comments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CommentsApi, CommentDetailsApi
-
-urlpatterns = [
-    path('post/', CommentsApi.as_view(), name="post"),
-    path('getComments/', CommentsApi.as_view(), name="getComment"),
-    path('deleteComments/<int:pk>/', CommentDetailsApi.as_view(), name="coment-detail")
-    # path("getComments/",CommentsApi.as_view(), name="getComments")
-]
